41a7823caa5cbf7da5e6225708574bad39473232
KevinMaggi <KevinMaggi@users.noreply.github.com>
2021-07-12 17:31:03 +0200

    Update GPIOLib.c

--- GPIOLib/GPIOLib.c
+++ GPIOLib/GPIOLib.c
@@ -11,7 +11,7 @@
 #include <stdlib.h>
 #include <sysLib.h>
 #include <subsys/gpio/vxbGpioLib.h>
-#include "gpioLib.h"
+#include "GPIOLib.h"
 
 
 /*******************************************************************************
