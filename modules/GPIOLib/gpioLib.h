0ecd346520d856684ddfac22bdeb80a79be5ddf3
KevinMaggi <KevinMaggi@users.noreply.github.com>
2021-07-12 17:07:22 +0200

    Update and rename modules/gpioLib.h to modules/GPIOLib/gpioLib.h

--- modules/GPIOLib/gpioLib.h
+++ modules/GPIOLib/gpioLib.h
@@ -1,15 +1,3 @@
-/* gpioLib.h - GPIO API definitions header file */
-
-/*
- * Copyright (c) 2019 Wind River Systems, Inc.
- *
- * The right to copy, distribute, modify or otherwise make use
- * of this software may be licensed only pursuant to the terms
- * of an applicable Wind River license agreement.
- */
-
-
-
 #ifndef __INCgpioLibH
 #define __INCgpioLibH
 #include "vxWorks.h"
@@ -21,10 +9,11 @@
 
 #define PINMODE_OUT		1
 #define PINMODE_IN		0
-#define OUTMODE_HIGH	1
-#define OUTMODE_LOW		0
+#define OUTMODE_HIGH    1
+#define OUTMODE_LOW     0
+#define INMODE_HIGH	    1
+#define INTMODE_LOW		0
 
-/*The only interface required right now to use the GPIO APIs*/
 typedef struct gpioSetStruct
 {
     UINT32 gpioNum;
